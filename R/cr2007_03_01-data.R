b89f87548d7ad7cc048e324e3ef49a76e161b0a1
Noah Greifer <noah.greifer@gmail.com>
2022-11-09 16:40:48 -0500

    Added documentation for datasets and package

---
+++ R/cr2007_03_01-data.R
@@ -0,0 +1,12 @@
+#' @title Text of the Congressional Record from 3/1/2007
+#' @description Text of the Congressional Record from 3/1/2007, scraped and parsed using methods described [here](https://github.com/judgelord/cr).
+#' @format A data frame with 154 rows and 5 variables:
+#' \describe{
+#'   \item{\code{date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{speaker}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{header}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{url}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{url_txt}}{character COLUMN_DESCRIPTION}
+#'}
+#' @details DETAILS
+"cr2007_03_01"
---
+++ R/legislators-package.R
@@ -0,0 +1,7 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import magrittr
+## usethis namespace: end
+NULL
